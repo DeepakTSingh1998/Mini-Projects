14e7ccffab1eedd917825c0a6526cde5ab64ac89
Deepak Singh <86015084+DeepakTSingh1998@users.noreply.github.com>
2022-07-05 11:16:19 +0100

    Create TieRopes.py

---
+++ Codility-Practice/GreedyAlgorithms/TieRopes.py
@@ -0,0 +1,11 @@
+def solution(K, A):
+    sum_rope = 0
+    counter = 0
+    for i in A:
+        sum_rope += i
+        if sum_rope >= K:
+            counter +=1
+            su = 0
+ 
+    return cnt
+    
